4eef6e885dbc065d9c9fe328ee11ce758ef4c96f
Jake Almeda <jakealmeda@gmail.com>
2020-11-19 00:40:28 +0800

    Changed Subscribe to CTAs

--- urc-subscribe.php
+++ urc-subscribe.php
@@ -13,8 +13,10 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly
 }
 
-add_action( 'genesis_before_content_sidebar_wrap', 'urc_subscribe_function' );
+//add_action( 'genesis_before_content_sidebar_wrap', 'urc_subscribe_function' ); // original
+add_action( 'genesis_before_content', 'urc_subscribe_function' );
 function urc_subscribe_function() {
+
 	?>
 	<style>
 		@media screen and (min-width: 768px) {
@@ -29,47 +31,52 @@ function urc_subscribe_function() {
 	</style>
 
 	<aside class="module subscribe">
-	<div class="item-subscribe">
-	<div class="pretitle"><span class="fontsize-xsml">For A</span> <span class="fontsize-sml">LIMITED TIME ONLY</span><br><span class="fontsize-xsml">Get </span> <span class="fontsize-sml">FREE</span> <span class="fontsize-xsml">Copies Of My</span></div>
-	  <div class="photo"></div>
-	  <div class="title"><span class="fontsize-med">Enter Your Name &amp; Email Below for Instant Access:</span></div>
-
-	<!-- Begin Mailchimp Signup Form -->
-	<link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
-	<!--style type="text/css">
-	  #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
-	  /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
-	     We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-	</style-->
-	<div id="mc_embed_signup">
-	<form action="https://understandingrelationships.us2.list-manage.com/subscribe/post?u=8fcb0ea8d36b15793f40f7ee8&amp;id=0463614dd6" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate="">
-	    <div id="mc_embed_signup_scroll">
-	  
-	<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
-	<div class="mc-field-group">
-	  <label for="mce-EMAIL">Email  <span class="asterisk">*</span>
-	</label>
-	  <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
-	</div>
-	<div class="mc-field-group">
-	  <label for="mce-FNAME">Name   <span class="asterisk">*</span>
-	</label>
-	  <input type="text" value="" name="FNAME" class="" id="mce-FNAME">
-	</div>
-	  <div id="mce-responses" class="clear">
-	    <div class="response" id="mce-error-response" style="display:none"></div>
-	    <div class="response" id="mce-success-response" style="display:none"></div>
-	  </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-	    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_8fcb0ea8d36b15793f40f7ee8_0463614dd6" tabindex="-1" value=""></div>
-	    <div class="clear formsubmit"><input type="submit" value="Instant Access" name="subscribe" id="mc-embedded-subscribe" class="button" onclick="return gtag_report_conversion('https://understandingrelationships.com/members-section')"></div>
-	    </div>
-	</form>
-	</div>
-
-	<!--End mc_embed_signup-->
-
-	<div class="disclaimer">Enter your name &amp; email in the boxes above to gain access to FREE Digital Online Versions of my popular eBooks &amp; audio course. When you click the “Instant Access” button, you will gain access to the members area of my website to read my eBooks, &amp; listen to the audio lessons right in your web browser! You’ll also get my best pickup, dating, relationship &amp; life success secrets &amp; strategies in my FREE newsletter. All information is 100% confidential. “Employ your time in improving yourself by other men’s writings, so that you shall gain easily what others have labored hard for.” ~ Socrates. “The man who doesn’t read good books has no advantage over the man who can’t read them.” ~ Mark Twain</div>
-	</div>
-	</aside>
 	<?php
+
+		$donate_page = get_permalink( '26341' ); // there might be more than 1 donate page with the same slug
+
+		$upload_dir = wp_upload_dir();
+		$free_ebook_page = get_permalink( get_page_by_path( "free-ebook" ) );
+		$products_page = get_permalink( get_page_by_path( "products" ) );
+
+    	$list_pages = array(
+    		'members-section',
+    		'free-ebook',
+    		'bookone-audio',
+    		'booktwo-ebook',
+    	);
+
+    	global $post; //$post->ID
+    	if( in_array( $post->post_name, $list_pages ) ) {
+    		$hide_on_these_pages = '';
+    	} else {
+    		$hide_on_these_pages = '<div class="module cta-main margin-bottom"><div class="module-wrap">
+										<div><a class="item image link" href="'.$free_ebook_page.'" data-type="page" data-id="1536" data-id="freeebook"><img src="'.$upload_dir[ "baseurl" ].'/cta-mobile-free-ebook.jpg" alt="" class="wp-image-41159"/></a></div>
+										<div class="items info"><h2><a class="item title link" href="'.$free_ebook_page.'">Get eBook for FREE!</a></h2><a class="item cta button" href="'.$free_ebook_page.'">CLICK HERE</a><div class="item info fontsize-smaller margin-smaller-top">* When you signup for our newsletter</div></div>
+									</div></div>
+									<div class="fontsize-tiny color-darkgray margin-bottom">Click on the link above to go to a page where you can enter your email to gain access to FREE Digital Online Versions of my popular eBooks &amp; audio course. When you click the “Instant Access” button, you will gain access to the members area of my website to read my eBooks, &amp; listen to the audio lessons right in your web browser! You’ll also get my best pickup, dating, relationship &amp; life success secrets &amp; strategies in my FREE newsletter. All information is 100% confidential.</div>';
+    	}
+
+		
+		$content = $hide_on_these_pages.'<div class="group grid-2columns margin-bottom">
+				<div class="module cta-icon"><div class="module-wrap">
+					<div><a class="item image link" href="'.$products_page.'" data-type="page" data-id="1519"><img src="'.$upload_dir[ "baseurl" ].'/cta-mobile-coaching-icon.png" alt="" class="wp-image-41163" width="50" height="50"></a></div><div class="items info"><h3><a class="item title link" href="'.$products_page.'" data-type="page" data-id="1519">Coaching</a></h3><div><a class="item cta link" href="'.$products_page.'" data-type="page" data-id="1519">Click To View</a></div></div>
+				</div></div>
+				<div class="module cta-icon"><div class="module-wrap">
+					<div><a class="item image link" href="https://teespring.com/stores/coach-corey-wayne"><img src="'.$upload_dir[ "baseurl" ].'/cta-mobile-products-icon.png" alt="" class="wp-image-41161" width="50" height="50"></a></div><div class="items info"><h3><a class="item title link" href="https://teespring.com/stores/coach-corey-wayne">Buy Merch</a></h3><div><a class="item cta link" href="https://teespring.com/stores/coach-corey-wayne">Click To View</a></div></div>
+				</div></div>
+				<div class="module cta-icon"><div class="module-wrap">
+					<div><a class="item image link" href="'.$donate_page.'" data-type="page" data-id="26341"><img src="'.$upload_dir[ "baseurl" ].'/cta-mobile-donate-icon.png" alt="" class="wp-image-41164" width="50" height="50"></a></div><div class="items info"><h3><a class="item title link" href="'.$donate_page.'" data-type="page" data-id="26341">Donate</a></h3><div><a class="item cta link" href="'.$donate_page.'" data-type="page" data-id="26341">Click To View</a></div></div>
+				</div></div>
+				<div class="module cta-icon"><div class="module-wrap">
+					<div><a class="item image link" href="'.$products_page.'" data-type="page" data-id="1519"><img src="'.$upload_dir[ "baseurl" ].'/cta-mobile-all-books-icon.png" alt="" class="wp-image-41162" width="50" height="50"></a></div><div class="items info"><h3><a class="item title link" href="'.$products_page.'" data-type="page" data-id="1519">All Books</a></h3><div><a class="item cta link" href="'.$products_page.'" data-type="page" data-id="1519">Click To View</a></div></div>
+				</div></div>
+			</div>';
+
+	    $content = apply_filters( 'the_content', $content );
+	    $content = str_replace( ']]>', ']]&gt;', $content );
+	    echo $content;
+
+	?></aside><?php
+
 }
